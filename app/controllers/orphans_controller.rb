71a5192a1530b01d5ddb9dade9340eaf25c22bbf
Cody Swann <cody@gunnertech.com>
2015-03-29 21:51:59 -0400

    minor

--- app/controllers/orphans_controller.rb
+++ app/controllers/orphans_controller.rb
@@ -1,13 +1,19 @@
 class OrphansController < InheritedResources::Base
-
+  
+  before_filter :set_organization_id, only: :create
+  
   private
 
   def orphan_params
-    params.require(:orphan).permit(:first_name, :last_name, :hashtag, :photo, :avatar, :description)
+    params.require(:orphan).permit(:first_name, :last_name, :hashtag, :photo, :avatar, :description, :organization_id)
   end
   
-  # def begin_of_association_chain
-  #   @organization
-  # end
+  def begin_of_association_chain
+    @organization
+  end
+  
+  def set_organization_id
+    params[:orphan][:organization_id] = @organization.id
+  end
 end
 
