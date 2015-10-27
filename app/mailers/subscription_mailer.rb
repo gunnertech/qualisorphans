de6f3012c8d3cdba3bc2ba27002414dbf270c1dd
Cody Swann <cody@gunnertech.com>
2015-10-26 21:18:21 -0400

    minor

--- app/mailers/subscription_mailer.rb
+++ app/mailers/subscription_mailer.rb
@@ -2,6 +2,6 @@ class SubscriptionMailer < ApplicationMailer
   def confirmation_email(user,orphan)
     @user = user
     @orphan = orphan
-    mail(to: @user.email, subject: "Thank you for supporting #{orphan.organization.to_s}")
+    mail(to: @user.email, bcc: "SamPetteway@suddenlink.net", subject: "Thank you for supporting #{orphan.organization.to_s}")
   end
 end
