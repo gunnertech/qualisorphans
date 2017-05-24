384052adbb5792c1053310bd4af231a0f6ef472c
Cody Swann <cody@gunnertech.com>
2017-05-24 12:13:58 -0400

    minor

--- config/initializers/fog.rb
+++ config/initializers/fog.rb
@@ -4,7 +4,7 @@ CarrierWave.configure do |config|
     :aws_access_key_id      => ENV['S3_KEY'],                        # required
     :aws_secret_access_key  => ENV['S3_SECRET'],                        # required
     :region                 => ENV['S3_REGION'],                  # optional, defaults to 'us-east-1'
-    # :host                   => 's3.example.com',             # optional, defaults to nil
+    :host                   => 's3-us-west-2.amazonaws.com',             # optional, defaults to nil
     # :endpoint               => 'https://s3.example.com:8080' # optional, defaults to nil
   }
   config.fog_directory  = ENV['S3_BUCKET']                     # required
